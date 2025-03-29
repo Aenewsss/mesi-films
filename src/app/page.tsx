f9e9fc7c36aa74a35e078ac9cbcd46187771d3c6
Aenewsss <aenamartinelli@gmail.com>
2025-03-29 13:31:59 -0300

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -105,6 +105,15 @@ export default function MesiFilms() {
 
       {/* Traffic Videos Section */}
       <section className="container mx-auto px-4 py-16">
+        <h2 className="text-center text-6xl  font-medium mb-16 relative">
+          <span className="relative px-8 inline-block">
+            VÍDEOS DE PERFORMANCE
+            <span className="absolute -left-6 -top-2 h-6 w-6 border-l-2 border-t-2 border-yellow-400"></span>
+            <span className="absolute -right-6 -top-2 h-6 w-6 border-r-2 border-t-2 border-yellow-400"></span>
+            <span className="absolute -left-6 -bottom-2 h-6 w-6 border-l-2 border-b-2 border-yellow-400"></span>
+            <span className="absolute -right-6 -bottom-2 h-6 w-6 border-r-2 border-b-2 border-yellow-400"></span>
+          </span>
+        </h2>
         <h2 className="text-center text-xl mb-2">VÍDEOS QUE VÃO PARA TRÁFEGO PAGO,</h2>
         <h3 className="text-center text-xl mb-12">
           COM O OBJETIVO DE TE FAZER <span className="text-yellow-400">VENDER MAIS</span>.
@@ -112,37 +121,76 @@ export default function MesiFilms() {
 
         <div className="relative max-w-4xl mx-auto">
           <div className="flex justify-center">
-            {activeVideoSlide === 0 && (
-              <div className="w-64 h-96 mx-2">
-                <Image
-                  src="/placeholder.svg?height=384&width=256"
-                  alt="Video example"
-                  width={256}
-                  height={384}
+          {activeVideoSlide === 0 && (
+              <div className="w-[481px] h-[857px] mx-2">
+                <video
+                  controls
+                  autoPlay
+                  src="https://firebasestorage.googleapis.com/v0/b/safrasul-sementes-fb9bd.appspot.com/o/mesi-films%2FCriativo%202%20-%20E%CC%81rica.mp4?alt=media&token=511396e3-86b5-4ac1-b702-530dc58fcf0a"
+                  width={481}
+                  height={857}
                   className="object-cover rounded-lg"
                 />
               </div>
             )}
 
             {activeVideoSlide === 1 && (
-              <div className="w-64 h-96 mx-2">
-                <Image
-                  src="/placeholder.svg?height=384&width=256"
-                  alt="Video example"
-                  width={256}
-                  height={384}
+              <div className="w-[481px] h-[857px] mx-2">
+                <video
+                  controls
+                  autoPlay
+                  src="https://firebasestorage.googleapis.com/v0/b/safrasul-sementes-fb9bd.appspot.com/o/mesi-films%2FFique%20quieto%20a%20nada%20vai%20acontecer.mp4?alt=media&token=883bb737-48a2-4b6f-8c8f-7bd84954e249"
+                  width={481}
+                  height={857}
                   className="object-cover rounded-lg"
                 />
               </div>
             )}
 
             {activeVideoSlide === 2 && (
-              <div className="w-64 h-96 mx-2">
-                <Image
-                  src="/placeholder.svg?height=384&width=256"
-                  alt="Video example"
-                  width={256}
-                  height={384}
+              <div className="w-[481px] h-[857px] mx-2">
+                <video
+                  controls
+                  autoPlay
+                  src="https://firebasestorage.googleapis.com/v0/b/safrasul-sementes-fb9bd.appspot.com/o/mesi-films%2FiPhone%2016.mp4?alt=media&token=8f718cca-94ed-4105-bdcd-84356c259e41"
+                  width={481}
+                  height={857}
+                  className="object-cover rounded-lg"
+                />
+              </div>
+            )}
+            {activeVideoSlide === 3 && (
+              <div className="w-[481px] h-[857px] mx-2">
+                <video
+                  controls
+                  autoPlay
+                  src="https://firebasestorage.googleapis.com/v0/b/safrasul-sementes-fb9bd.appspot.com/o/mesi-films%2FMetabolismo%20influencia%20suas%20emoc%CC%A7o%CC%83es%20Patrocinado.mp4?alt=media&token=d7f9373a-5ba3-4170-b47e-b5a4e37b1dc6"
+                  width={481}
+                  height={857}
+                  className="object-cover rounded-lg"
+                />
+              </div>
+            )}
+            {activeVideoSlide === 4 && (
+              <div className="w-[481px] h-[857px] mx-2">
+                <video
+                  controls
+                  autoPlay
+                  src="https://firebasestorage.googleapis.com/v0/b/safrasul-sementes-fb9bd.appspot.com/o/mesi-films%2FVoce%CC%82%20mora%20em%20Brasi%CC%81lia.mp4?alt=media&token=bd90f198-0dcb-4631-a9dd-547414235584"
+                  width={481}
+                  height={857}
+                  className="object-cover rounded-lg"
+                />
+              </div>
+            )}
+            {activeVideoSlide === 5 && (
+              <div className="w-[481px] h-[857px] mx-2">
+                <video
+                  controls
+                  autoPlay
+                  src="https://firebasestorage.googleapis.com/v0/b/safrasul-sementes-fb9bd.appspot.com/o/mesi-films%2FCriativo%203%20VSP.mp4?alt=media&token=cf8f6e2e-773f-4bca-b49e-15e36472b5e6"
+                  width={481}
+                  height={857}
                   className="object-cover rounded-lg"
                 />
               </div>
@@ -167,6 +215,15 @@ export default function MesiFilms() {
 
       {/* Feed Videos Section */}
       <section className="container mx-auto px-4 py-16">
+        <h2 className="text-center text-6xl  font-medium mb-16 relative">
+          <span className="relative px-8 inline-block">
+            VÍDEOS DE BRANDING
+            <span className="absolute -left-6 -top-2 h-6 w-6 border-l-2 border-t-2 border-yellow-400"></span>
+            <span className="absolute -right-6 -top-2 h-6 w-6 border-r-2 border-t-2 border-yellow-400"></span>
+            <span className="absolute -left-6 -bottom-2 h-6 w-6 border-l-2 border-b-2 border-yellow-400"></span>
+            <span className="absolute -right-6 -bottom-2 h-6 w-6 border-r-2 border-b-2 border-yellow-400"></span>
+          </span>
+        </h2>
         <h2 className="text-center text-xl mb-2">VÍDEOS QUE VÃO PARA O FEED,</h2>
         <h3 className="text-center text-xl mb-12">
           COM O OBJETIVO DE TORNAR O SEU PERFIL <span className="text-yellow-400">MAIS ATRATIVO</span>.
@@ -175,36 +232,75 @@ export default function MesiFilms() {
         <div className="relative max-w-4xl mx-auto">
           <div className="flex justify-center">
             {activeFeedSlide === 0 && (
-              <div className="w-64 h-96 mx-2">
-                <Image
-                  src="/placeholder.svg?height=384&width=256"
-                  alt="Feed video example"
-                  width={256}
-                  height={384}
+              <div className="w-[481px] h-[857px] mx-2">
+                <video
+                  controls
+                  autoPlay
+                  src="https://firebasestorage.googleapis.com/v0/b/safrasul-sementes-fb9bd.appspot.com/o/mesi-films%2F2%20opc%CC%A7o%CC%83es%20Finalizado.mp4?alt=media&token=c3646407-8c7f-4665-bc51-9fa4dd02e29b"
+                  width={481}
+                  height={857}
                   className="object-cover rounded-lg"
                 />
               </div>
             )}
 
             {activeFeedSlide === 1 && (
-              <div className="w-64 h-96 mx-2">
-                <Image
-                  src="/placeholder.svg?height=384&width=256"
-                  alt="Feed video example"
-                  width={256}
-                  height={384}
+              <div className="w-[481px] h-[857px] mx-2">
+                <video
+                  controls
+                  autoPlay
+                  src="https://firebasestorage.googleapis.com/v0/b/safrasul-sementes-fb9bd.appspot.com/o/mesi-films%2FEntrega%20premium.mp4?alt=media&token=cd4bb2e2-4750-4ec7-ae13-044d7d28670d"
+                  width={481}
+                  height={857}
                   className="object-cover rounded-lg"
                 />
               </div>
             )}
 
             {activeFeedSlide === 2 && (
-              <div className="w-64 h-96 mx-2">
-                <Image
-                  src="/placeholder.svg?height=384&width=256"
-                  alt="Feed video example"
-                  width={256}
-                  height={384}
+              <div className="w-[481px] h-[857px] mx-2">
+                <video
+                  controls
+                  autoPlay
+                  src="https://firebasestorage.googleapis.com/v0/b/safrasul-sementes-fb9bd.appspot.com/o/mesi-films%2FQuais%20sinais%20-%20Leso%CC%83es.mp4?alt=media&token=233478c0-9c71-4613-9d54-ccda6998386d"
+                  width={481}
+                  height={857}
+                  className="object-cover rounded-lg"
+                />
+              </div>
+            )}
+            {activeFeedSlide === 3 && (
+              <div className="w-[481px] h-[857px] mx-2">
+                <video
+                  controls
+                  autoPlay
+                  src="https://firebasestorage.googleapis.com/v0/b/safrasul-sementes-fb9bd.appspot.com/o/mesi-films%2FSuco%20para%20emagrecer.mp4?alt=media&token=ff922a34-4ee2-4f9a-9d61-f9c5f3c56966"
+                  width={481}
+                  height={857}
+                  className="object-cover rounded-lg"
+                />
+              </div>
+            )}
+            {activeFeedSlide === 4 && (
+              <div className="w-[481px] h-[857px] mx-2">
+                <video
+                  controls
+                  autoPlay
+                  src="https://firebasestorage.googleapis.com/v0/b/safrasul-sementes-fb9bd.appspot.com/o/mesi-films%2FTipos%20de%20treinamentos.mp4?alt=media&token=9158dd82-0240-48e6-a7ce-c15455d944f9"
+                  width={481}
+                  height={857}
+                  className="object-cover rounded-lg"
+                />
+              </div>
+            )}
+            {activeFeedSlide === 5 && (
+              <div className="w-[481px] h-[857px] mx-2">
+                <video
+                  controls
+                  autoPlay
+                  src="https://firebasestorage.googleapis.com/v0/b/safrasul-sementes-fb9bd.appspot.com/o/mesi-films%2FTreino%20graduados%202.mp4?alt=media&token=7da00e3e-2eb6-453f-a4c2-cdeb05b843cc"
+                  width={481}
+                  height={857}
                   className="object-cover rounded-lg"
                 />
               </div>
@@ -229,7 +325,7 @@ export default function MesiFilms() {
 
       {/* Testimonials Section */}
       <section className="container mx-auto px-4 py-16">
-        <h2 className="text-center text-2xl font-bold mb-16 relative">
+        <h2 className="text-center text-6xl  font-medium mb-16 relative">
           <span className="relative px-8 inline-block">
             DEPOIMENTOS
             <span className="absolute -left-6 -top-2 h-6 w-6 border-l-2 border-t-2 border-yellow-400"></span>
